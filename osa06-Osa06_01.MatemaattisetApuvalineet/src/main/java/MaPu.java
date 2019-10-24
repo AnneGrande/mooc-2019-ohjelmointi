f506576a479c4e4581ccd34ed93c99b75b1685c6
AnneGrande <54155765+AnneGrande@users.noreply.github.com>
2019-10-24 09:31:39 +0300

    Add files via upload

---
+++ osa06-Osa06_01.MatemaattisetApuvalineet/src/main/java/MaPu.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+public class MaPu {
+
+    public static double keskiarvo(ArrayList<Integer> luvut) {
+        int i = 0;
+        int summa = 0;
+        while (i < luvut.size()) {
+            summa = summa + luvut.get(i);
+            i++;
+        }
+        return 1.0 * summa / luvut.size();
+    }
+}
---
+++ osa06-Osa06_03.Lempinimet/src/main/java/Lempinimet.java
@@ -0,0 +1,17 @@
+
+import java.util.HashMap;
+
+public class Lempinimet {
+
+    public static void main(String[] args) {
+        
+    HashMap<String, String> lempinimet = new HashMap<>();
+    lempinimet.put("Matti", "Mage");
+    lempinimet.put("Mikael", "Mixu");
+    lempinimet.put("Arto", "Arppa");
+
+    System.out.println(lempinimet.get("Mikael"));
+    
+    }
+
+}
