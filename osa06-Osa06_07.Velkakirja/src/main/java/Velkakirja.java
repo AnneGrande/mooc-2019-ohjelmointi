83bdff2ef451e09c642d90eb7c4092026792f8d8
AnneGrande <54155765+AnneGrande@users.noreply.github.com>
2019-10-24 09:36:16 +0300

    Add files via upload

---
+++ osa06-Osa06_07.Velkakirja/src/main/java/Velkakirja.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anneisola
+ */
+import java.util.HashMap;
+public class Velkakirja {
+    private HashMap<String, Double> velka;
+    
+    public Velkakirja () {
+        this.velka = new HashMap<>();
+    }
+    
+    public void asetaLaina(String kenelle, double maara) {
+        this.velka.put(kenelle, maara * 1.0);
+    }
+    
+    public double paljonkoVelkaa(String kenelle) {
+        return this.velka.getOrDefault(kenelle, 0.0);
+        }  
+}
