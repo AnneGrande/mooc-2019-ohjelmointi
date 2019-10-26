13082bd860881d553aedce8606b9829097614758
AnneGrande <54155765+AnneGrande@users.noreply.github.com>
2019-10-26 16:50:17 +0300

    Add files via upload

--- osa07-Osa07_09.LintubongarinTietokanta/src/main/java/Paaohjelma.java
+++ osa07-Osa07_09.LintubongarinTietokanta/src/main/java/Paaohjelma.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Paaohjelma {
 
@@ -9,7 +10,47 @@ public class Paaohjelma {
         // tarvittaessa parametrina.
 
         Scanner lukija = new Scanner(System.in);
-
+        ArrayList <Lintu> linnut = new ArrayList<>();
+        
+        while(true) {
+            System.out.print("?");
+            String komento = lukija.nextLine();
+            if (komento.equals("Lopeta")) break;
+            if (komento.equals("Lisaa")){
+                System.out.print("Nimi: ");
+                String nimi = lukija.nextLine();
+                System.out.print("Latinankielinen nimi: ");
+                String latinaksi = lukija.nextLine();
+                Lintu lisattava = new Lintu(nimi, latinaksi);
+                linnut.add(lisattava);
+                continue;
+            }
+            if (komento.equals("Havainto")) {
+                System.out.print("Mikä havaittu? ");
+                String havaittu = lukija.nextLine();
+                for (Lintu lintu : linnut) {
+                    if (lintu.getLaji().equals(havaittu)) {
+                        lintu.havainto();
+                    }
+                } continue;
+            }
+            if (komento.equals("Tilasto")) {
+                for (Lintu lintu : linnut) {
+                    System.out.println(lintu);
+                } continue;
+            }
+            if (komento.equals("Nayta")) {
+                System.out.print("Mikä? ");
+                String naytettava = lukija.nextLine();
+                for (Lintu lintu : linnut) {
+                    if (lintu.getLaji().equals(naytettava)) {
+                        System.out.println(lintu);
+                        ;
+                    }
+                }
+            }
+        }
     }
-
 }
+
+        
