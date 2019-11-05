7e25930a2d7a75f2b85cb9b6e0d1cd0b3a5f9d5b
AnneGrande <54155765+AnneGrande@users.noreply.github.com>
2019-11-05 13:40:42 +0200

    Add files via upload

---
+++ osa08-Osa08_04.ErilaisiaLaatikoita/src/main/java/HukkaavaLaatikko.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anneisola
+ */
+import java.util.ArrayList;
+public class HukkaavaLaatikko extends Laatikko {
+    
+    public HukkaavaLaatikko() {
+        this.tavarat = new ArrayList<>();
+    }
+    
+    @Override
+    public boolean onkoLaatikossa(Tavara tavara) {
+            return false;
+    }
+    
+    @Override
+    public void lisaa(Tavara tavara) {
+        this.tavarat.add(tavara);
+    }
+    
+}
---
+++ osa08-Osa08_05.Palvelusvelvollinen/src/main/java/Sivari.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anneisola
+ */
+public class Sivari implements Palvelusvelvollinen{
+    
+    private int aamuja;
+    
+    public Sivari() {
+        this.aamuja = 362;
+    }
+    
+    public int paiviaJaljella() {
+        return this.aamuja;
+    }
+    
+    public void palvele() {
+        if (this.aamuja >= 1) this.aamuja -= 1;
+    }   
+}
