808b0406d2551d1f230ada3caf4843150020a83f
AnneGrande <54155765+AnneGrande@users.noreply.github.com>
2019-11-08 10:44:12 +0200

    Add files via upload

---
+++ osa09-Osa09_10.KirjatTiedostosta/src/main/java/KirjatTiedostosta.java
@@ -0,0 +1,30 @@
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class KirjatTiedostosta {
+    
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        // testaa metodia täällä
+        
+    }
+        public static List<Kirja> lueKirjat(String tiedosto) {
+        List<Kirja> palautetaanKirjat= new ArrayList<>();
+        try {
+        Files.lines(Paths.get(tiedosto))
+                .map(rivi -> rivi.split(","))
+                .filter(palat -> palat.length >= 2)
+                .map (palat -> new Kirja(palat[0], Integer.valueOf(palat[1]), 
+                        Integer.valueOf(palat[2]), palat[3]))
+                .forEach(kirja -> palautetaanKirjat.add(kirja));
+                } catch (Exception e) {
+            System.out.println("Virhe: " + e.getMessage());
+        }
+        return palautetaanKirjat;
+    }
+}
