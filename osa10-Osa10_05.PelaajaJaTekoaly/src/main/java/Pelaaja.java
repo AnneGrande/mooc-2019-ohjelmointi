edd04ebec4fddb011e3ae6bc2c53e9a99f460d6f
AnneGrande <54155765+AnneGrande@users.noreply.github.com>
2019-11-22 09:35:15 +0200

    Add files via upload

---
+++ osa10-Osa10_05.PelaajaJaTekoaly/src/main/java/Pelaaja.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anneisola
+ */
+public class Pelaaja {
+    private String nimi;
+    
+
+public void pelaa(){
+    System.out.println("Paina sininen kolme kertaa sitten punainen! ");
+
+}
+
+public void tulostaNimi(){
+    System.out.println(this.nimi);
+}
+}
+
