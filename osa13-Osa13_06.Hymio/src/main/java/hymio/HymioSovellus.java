4a8e9b5af58d0a1dc41661f52942dfd71e991ddb
AnneGrande <54155765+AnneGrande@users.noreply.github.com>
2019-12-16 17:47:29 +0200

    Add files via upload

---
+++ osa13-Osa13_06.Hymio/src/main/java/hymio/HymioSovellus.java
@@ -0,0 +1,38 @@
+package hymio;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+
+public class HymioSovellus extends Application {
+
+    
+    public static void main(String[] args) {
+        launch(HymioSovellus.class);
+    }
+    
+    @Override
+    public void start(Stage ikkuna) {
+        BorderPane asettelu = new BorderPane();
+        Canvas piirtoalusta = new Canvas(600, 500);
+        GraphicsContext piirturi = piirtoalusta.getGraphicsContext2D();
+        
+        asettelu.setCenter(piirtoalusta);
+        piirturi.setFill(Color.BLACK);
+       
+        piirturi.fillRect(150, 100, 75, 75);
+        piirturi.fillRect(400, 100, 75, 75);
+        piirturi.fillArc(200, 250, 200, 170, 180, 180, ArcType.OPEN);
+        
+        Scene nakyma = new Scene(asettelu);
+
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+        
+    }
+}
